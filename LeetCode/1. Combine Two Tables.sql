ad7a1d7d7970a98f17b8195476b88a801796fc92
Mustakim Alam Moon <mustakimalammoon@gmail.com>
2025-10-06 00:52:47 +0600

    Comment changes

--- LeetCode/1. Combine Two Tables.sql
+++ LeetCode/1. Combine Two Tables.sql
@@ -1,5 +1,4 @@
 -- Method-1: Left Join - on
-
 select firstName, lastName, city, state
 from Person p
 left join Address a
@@ -7,7 +6,6 @@ on p.personId = a.personId
 
 
 -- Method-2: Left Join - using
-
 select p.firstName, p.lastName, a.city, a.state
 from Person p
 left join Address a using (personId)
