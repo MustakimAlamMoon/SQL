2200ca0b60d0312f0fe1c0cfdfa42bf6efb15cd8
Mustakim Alam Moon <mustakimalammoon@gmail.com>
2025-10-06 00:51:29 +0600

    Add solution for "Combine Two Tables"

---
+++ LeetCode/1. Combine Two Tables.sql
@@ -0,0 +1,13 @@
+-- Method-1: Left Join - on
+
+select firstName, lastName, city, state
+from Person p
+left join Address a
+on p.personId = a.personId
+
+
+-- Method-2: Left Join - using
+
+select p.firstName, p.lastName, a.city, a.state
+from Person p
+left join Address a using (personId)
