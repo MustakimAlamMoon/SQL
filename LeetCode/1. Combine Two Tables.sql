9250c41d5b5d70d32ae2be4cc4672f604b41a257
Mustakim Alam Moon <mustakimalammoon@gmail.com>
2025-10-06 22:55:39 +0600

    Changes method types

--- LeetCode/1. Combine Two Tables.sql
+++ LeetCode/1. Combine Two Tables.sql
@@ -1,11 +1,11 @@
--- Method-1: Left Join - on
+-- Method-1: Left Join - on (Best)
 select firstName, lastName, city, state
 from Person p
 left join Address a
 on p.personId = a.personId
 
 
--- Method-2: Left Join - using
+-- Method-2: Left Join - using (Best)
 select p.firstName, p.lastName, a.city, a.state
 from Person p
 left join Address a using (personId)
